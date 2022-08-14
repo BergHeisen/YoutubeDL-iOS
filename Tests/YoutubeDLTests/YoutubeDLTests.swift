cbd3549e98771b30398ecf96c2fe96f1156e0025
BergHeisen <43702465+BergHeisen@users.noreply.github.com>
2022-08-14 02:32:22 +0200

    Update YoutubeDLTests.swift

--- Tests/YoutubeDLTests/YoutubeDLTests.swift
+++ Tests/YoutubeDLTests/YoutubeDLTests.swift
@@ -20,12 +20,49 @@ final class YoutubeDLTests: XCTestCase {
         }
     }
     
-    func textInfoExtraction() async throws {
+    
+    func testInfoExtraction() async throws {
         let ytdl = try YoutubeDL()
-        let (formats, info) = try ytdl.extractInfo(url: URL.init(string: "https://www.youtube.com/watch?v=WPiEbYSF9kE")!)
+        let (formats, info) = try ytdl.extractInfo(url: URL.init(string: "https://www.youtube.com/watch?v=t_jHrUE5IOk")!)
         XCTAssertNotNil(info)
         
     }
+    
+    func testVideoDownload() async throws {
+        let url = "https://www.youtube.com/watch?v=t_jHrUE5IOk"
+        
+        guard let directory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("org.bergheisen.video-utilities") else {
+            fatalError()
+        }
+        guard let _ = try? FileManager.default.createDirectory(at: directory, withIntermediateDirectories: true, attributes: nil) else {
+            fatalError()
+        }
+        
+        let destination = directory.appendingPathComponent("test.webm")
+        let ytdl = try YoutubeDL()
+        try await ytdl.downloadVideo(url: url, destination: destination, resolution: "360")
+        XCTAssertTrue(FileManager.default.fileExists(atPath: destination.path))
+    }
+    
+    func testProgressHook() async throws {
+        let ytdl = try YoutubeDL()
+        let url = "https://www.youtube.com/watch?v=t_jHrUE5IOk"
+        
+        guard let directory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("org.bergheisen.video-utilities") else {
+            fatalError()
+        }
+        guard let _ = try? FileManager.default.createDirectory(at: directory, withIntermediateDirectories: true, attributes: nil) else {
+            fatalError()
+        }
+        
+        let destination = directory.appendingPathComponent("test.webm")
+        let progressHook = directory.appendingPathComponent("progressHook")
+        try await ytdl.downloadVideo(url: url, destination: destination, resolution: "360", progressHookDestination: progressHook)
+        XCTAssertTrue(FileManager.default.fileExists(atPath: progressHook.path))
+        XCTAssertTrue(FileManager.default.fileExists(atPath: destination.path))
+        
+    }
+        
     override func tearDown() {
         PythonSupport.finalize()
     }
